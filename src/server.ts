44073c2379bdf7af8d309212aa6671a89bea389f
Bob <wend3llsouza@gmail.com>
2021-04-21 19:22:18 -0300

    :package: entitie and uuid generate seted up

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import express from 'express';
+import './database';
 
 const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello World"});
+});
+
+app.post("/", (req, res) => {
+  return res.json({ message: "User Resgistered" });
+});
 
 app.listen(3333, () => console.log('server running in port 3333'));
